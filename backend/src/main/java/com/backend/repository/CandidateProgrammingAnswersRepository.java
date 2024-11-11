e8a90b85a1821f046017cfe996dd0810ae5b4b54
brijeshpatel1011 <brijeshpatel1011.bg@gmail.com>
2024-11-11 18:27:39 +0530

    Schemas and Some Endpoints for Exam Creation

--- backend/src/main/java/com/backend/repository/CandidateProgrammingAnswersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CandidateProgrammingAnswersRepository extends JpaRepository<CandidateProgrammingAnswers, Long> {
-}
--- backend/src/main/java/com/backend/repository/ExamProgrammingQuestionsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExamProgrammingQuestionsRepository extends JpaRepository<ExamProgrammingQuestions, Long> {
-}
